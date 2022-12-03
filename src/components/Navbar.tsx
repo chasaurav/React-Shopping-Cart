9734b63ff40f9a6b507ad714590446f42c58174d
Saurav <chasaurav@gmail.com>
2022-12-03 20:23:13 +0530

    React shopping cart

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import { Button, Container, Nav, Navbar as NavbarBS } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import { useShoppingCart } from '../context/ShoppingCartContext'
+
+export function Navbar() {
+    const { openCart, closeCart, cartQuantity } = useShoppingCart();
+
+    return (
+        <NavbarBS sticky="top" className='bg-white mb-3 shadow-sm'>
+            <Container>
+                <Nav className='me-auto'>
+                    <Nav.Link to="/" as={NavLink}>
+                        Home
+                    </Nav.Link>
+                    <Nav.Link to="/store" as={NavLink}>
+                        Store
+                    </Nav.Link>
+                    <Nav.Link to="/about" as={NavLink}>
+                        About
+                    </Nav.Link>
+                </Nav>
+                {cartQuantity > 0 && (
+                    <Button
+                        onClick={openCart}
+                        style={{ width: '3rem', height: '3rem', position: 'relative' }}
+                        variant="outline-primary"
+                        className='rounded-circle'>
+                            Cart
+                            <div
+                                className='rounded-circle bg-danger d-flex justify-content-center align-items-center'
+                                style={{
+                                    width: '1.5rem',
+                                    color: 'white',
+                                    height: '1.5rem',
+                                    position: 'absolute',
+                                    bottom: 0,
+                                    right: 0,
+                                    transform: "translate(25%, 25%)"
+                                }}
+                                >{cartQuantity}</div>
+                        </Button>
+                )}
+            </Container>
+        </NavbarBS>
+    )
+}
---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,97 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+type ShoppingCartProviderProps = {
+    children: ReactNode
+}
+
+type CartItem = {
+    id: number,
+    quantity: number
+}
+
+type ShoppingCartContext = {
+    openCart: () => void
+    closeCart: () => void
+    getItemQuantity: (id: number) => number
+    increaseCartQuantity: (id: number) => void
+    decreaseCartQuantity: (id: number) => void
+    removeFromCart: (id: number) => void
+    cartQuantity: number
+    cartItems: CartItem[]
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", []);
+
+    function getItemQuantity(id:number) {
+        return cartItems.find(item => item.id == id)?.quantity || 0
+    }
+
+    function increaseCartQuantity(id:number) {
+        setCartItems(cartItems => {
+            if (cartItems.find(item => item.id == id) == null) {
+                return [...cartItems, { id, quantity: 1 }];
+            } else {
+                return cartItems.map(item => {
+                    if (item.id == id) {
+                        return { ...item, quantity: item.quantity + 1 }
+                    } else {
+                        return item;
+                    }
+                })
+            }
+        });
+        return cartItems.find(item => item.id == id)?.quantity || 0
+    }
+
+    function decreaseCartQuantity(id:number) {
+        setCartItems(cartItems => {
+            if (cartItems.find(item => item.id == id)?.quantity == 1) {
+                return cartItems.filter(item => item.id != id)
+            } else {
+                return cartItems.map(item => {
+                    if (item.id == id) {
+                        return { ...item, quantity: item.quantity - 1 }
+                    } else {
+                        return item;
+                    }
+                })
+            }
+        });
+        return cartItems.find(item => item.id == id)?.quantity || 0
+    }
+
+    function removeFromCart(id:number) {
+        setCartItems(cartItems => {
+            return cartItems.filter(item => item.id != id)
+        });
+    }
+
+    const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0);
+
+    const openCart = () => setIsOpen(true)
+    const closeCart = () => setIsOpen(false)
+
+    return <ShoppingCartContext.Provider value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+        openCart,
+        closeCart,
+        cartItems,
+        cartQuantity,
+    }}>
+        {children}
+        <ShoppingCart isOpen={isOpen} />
+    </ShoppingCartContext.Provider>
+}
